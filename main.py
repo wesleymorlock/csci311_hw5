9341c03313afb28b41b2eaf970a46a86c6a4bba4
wesleymorlock <wjm024@bucknell.edu>
2017-11-28 13:24:34 -0500

    Finished code
    Still needs to adjust where the files are being created

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ Main
 """
 from testFileWriter import *
 from max_flow_generator import *
+import os
 
 def main():
     for z in range(10):
@@ -15,7 +16,6 @@ def main():
 
         r = test_network_generator()
         vertices = r[1]
-        print("vertices: " + str(vertices) + "\n")
         vertices.remove('0')
         vertices.remove('6') ##sometime 6 ins't a vertex -> will give error
 
@@ -26,46 +26,24 @@ def main():
             g.addEdge(x[i][0],x[i][1],x[i][2])
             
         file_name = "my_graph_" + str(z)
-        write_dot_file(r[0], file_name)  
+        name = os.path.join("./input_graphs/", file_name)
+        print(file_name)
+        write_dot_file(r[0], name)  
 
         max_edges = g.getMaxEdges()
+
+        
 
         print('vertices: ' + str([vertex.name for vertex in g.vertices]) + '\n')
         print('edges: ' + str(['%s -> %s; %s/%s' % (e.start, e.end, e.flow, e.capacity) for e in g.getEdges()])+ '\n')
         print('max flow: ' + str(g.calculateMaxFlow()) + '\n')
         print('max edges: ' + str(max_edges))
+        print("----------------------------------------------------------------------------------------\n")
 
         file_name = "max_edges_" + str(z)
-        write_dot_file(max_edges, file_name)
+        name = os.path.join("./output_graphs/", file_name)
+        write_dot_file(max_edges, name)
+
+        
 
 main()
-
-def main() :
-   #assume, X is your graph.
-   
-       # X = [(1, 2, 4), (2, 3, 9), (3, 2, 4), (3, 3, 1)]
-       X = test_network_generator()
-       file_name = "my_graph_" + str(i)
-       write_dot_file(X, file_name)   
-   # done 
-
-
-
-##def main():
-##    g = FlowNetwork()
-##    g.addVertex('1', True, False)   ##source
-##    g.addVertex('6',False,True)     ##sink
-##    vertices = ['2', '3', '4', '5']
-##    for s in vertices:
-##        g.addVertex(s)
-##    g.addEdge('1', '2', 5)
-##    g.addEdge('1', '3', 10)
-##    g.addEdge('2', '4', 5)
-##    g.addEdge('3', '5', 6)
-##    g.addEdge('4', '6', 3)
-##    g.addEdge('4', '3', 2)
-##    g.addEdge('5', '2', 3)
-##    g.addEdge('5', '4', 2)
-##    g.addEdge('5', '6', 4)
-##    g.addEdge('2', '6', 3)
-##
